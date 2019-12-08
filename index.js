28fa41f8e07e19abe8dbdb0ca55f6fc0287523b6
Conrad <conrisc@gmail.com>
2019-12-08 20:09:54 +0100

    Adds demo gif

--- index.js
+++ index.js
@@ -160,6 +160,6 @@ function drawPath(p1, p2, d) {
 }
 
 function calculateDistance(p1, p2) {
-    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    return Math.floor(Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)));
 }
 
